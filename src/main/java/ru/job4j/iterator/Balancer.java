b475ca4d9f5ccfbffabe51104248105b45be8720
vsevolodgornostaev <sevlantiy@inbox.ru>
2024-03-31 15:48:45 +0300

    7. Балансир

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,18 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        while (source.hasNext()) {
+            for (int i = 0; i < nodes.size(); i++) {
+                if (source.hasNext()) {
+                    nodes.get(i).add(source.next());
+                }
+            }
+        }
+    }
+}
