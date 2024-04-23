31188517cfddb4915a64e9af168c052410072fd2
vsevolodgornostaev <sevlantiy@inbox.ru>
2024-04-23 06:42:54 +0300

    6. Перевернуть связанный список

---
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -0,0 +1,68 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class RevertLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public boolean revert() {
+        Node<T> node = head;
+        head = null;
+        if (node != null) {
+            if (node.next != null) {
+                while (node != null) {
+                    Node<T> tail = node.next;
+                    node.next = head;
+                    head = node;
+                    node = tail;
+                }
+            }
+        }
+        return head != null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
