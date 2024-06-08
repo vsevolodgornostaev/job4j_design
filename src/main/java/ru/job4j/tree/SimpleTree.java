676868acbbfe7fa896b771d200a87860dd9f22f7
vsevolodgornostaev <sevlantiy@inbox.ru>
2024-06-08 20:51:09 +0300

    1. Создать элементарную структуру дерева

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
+
     private final Node<E> root;
 
     public SimpleTree(final E root) {
