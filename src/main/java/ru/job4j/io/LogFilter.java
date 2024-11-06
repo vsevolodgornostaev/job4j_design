f1158867089fe97848eb1c3fab2b891611901a14
vsevolodgornostaev <sevlantiy@inbox.ru>
2024-11-06 21:10:36 +0300

    5.1. BufferedReader [#252489 #520232]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.io.*;
 
@@ -12,13 +11,12 @@ public class LogFilter {
         this.file = file;
     }
 
-    @SuppressWarnings({"checkstyle:EmptyBlock", "checkstyle:EmptyStatement"})
     public List<String> filter() {
         List<String> filteredList = new ArrayList<>();
         try (BufferedReader input = new BufferedReader(new FileReader(file))) {
             for (String line = input.readLine(); line != null; line = input.readLine()) {
                 String[] words = line.split(" ");
-                if (words[words.length - 2].equals("404")) {
+                if ("404".equals(words[words.length - 2])) {
                     filteredList.add(line);
                 }
             }
